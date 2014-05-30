53aa734b04bc6520926f933c598f44733e932a40
Calvin Metcalf <cmetcalf@appgeo.com>
2014-05-30 11:42:45 -0400

    fix city bug remove request

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 'use strict';
-var url = 'http://gisprpxy.itd.state.ma.us/MassGISCustomGeocodeLatLongApplication/MassGISCustomGeocodeService.asmx';
+
+var url = require('url');
+var http = require('http');
 var Handlebars = require('handlebars');
 var multiline = require('multiline');
-var request = require('request');
 var sax = require('sax');
+
+var endpoint = url.parse('http://gisprpxy.itd.state.ma.us/MassGISCustomGeocodeLatLongApplication/MassGISCustomGeocodeService.asmx');
+endpoint.method = 'post';
+endpoint.headers = {
+  'Content-type':'text/xml'
+};
 var template = Handlebars.compile(multiline(function () {/*
 <?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
     <GeocodeAddress xmlns="http://tempuri.org/">
       {{#if address}}<Address>{{address}}</Address>{{/if}}
-      {{#if city}}<City>city</City>{{/if}}
+      {{#if city}}<City>{{city}}</City>{{/if}}
       {{#if zip}}<ZipCode>{{zip}}</ZipCode>{{/if}}
       <State>MA</State>
     </GeocodeAddress>
   </soap:Body>
 </soap:Envelope>
 */}));
+
+
 module.exports = function (obj, callback) {
-  request({
-    url: url,
-    body: template(obj),
-    headers: {
-      'Content-type':'text/xml'
-    },
-    method: 'POST'
-  }, function (err, response, body) {
-    if (err) {
-      return callback(err);
+  var req = http.request(endpoint, function (res) {
+    if (res.statusCode !== 200) {
+      return callback(new Error('address not found'));
     }
     var out = {};
     var parser = sax.parser();
@@ -45,6 +47,13 @@ module.exports = function (obj, callback) {
       });
     };
     parser.onerror = callback;
-    parser.write(body).close();
-  });
+    res.on('data', function (d) {
+      parser.write(d.toString());
+    });
+    res.on('end', function () {
+      parser.end();
+    });
+  }).on('error', callback);
+  req.write(new Buffer(template(obj)));
+  req.end();
 };
